95ea635518370613fef1c199c821ba4593345d4b
Shiv_Shankar <165561744+Shivxr@users.noreply.github.com>
2025-03-07 21:42:22 +0530

    Update Path Sum II.py

--- Path Sum II.py
+++ Path Sum II.py
@@ -1,1 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], t: int) -> List[List[int]]:
+        if not root:
+            return []
+        else:
+            stck=[(root,root.val,[root.val])]
+            al=[]
 
+            while stck:
+
+                a,s,path=stck.pop()
+
+                if s==t and not a.left and not a.right:
+                    al.append(path)
+
+                if a.right:
+                    stck.append((a.right,s+a.right.val,path+[a.right.val]))
+                if a.left:
+                    stck.append((a.left,s+a.left.val,path+[a.left.val]))
+            
+            return al
+                
+            
+            
+
+        
+
+
+
+
+        
